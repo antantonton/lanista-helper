6ebfbd8bdec821fbc744bd7329ec8027ad185425
Anton <antonfahlgren1994@gmail.com>
2023-01-28 17:24:30 +0100

    WIP on injected overlay instead of popup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { TabService } from './shared/services/tab.service'
+import { ScriptService } from './shared/services/script.service'
 import { LANISTA_BASE_URL } from './shared/constants/lanista.constants'
 
 @Component({
@@ -8,9 +9,20 @@ import { LANISTA_BASE_URL } from './shared/constants/lanista.constants'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  readonly iframeId = 'lanista-helper'
+  readonly normalHeight: '64px'
+  readonly expandedHeight: '256px'
+
+  expanded = false
+
   lanistaTab: chrome.tabs.Tab | undefined
 
-  constructor(private _tabService: TabService) {}
+  iframe: HTMLIFrameElement | undefined
+
+  constructor(
+    private _tabService: TabService,
+    private _scriptService: ScriptService,
+  ) {}
 
   ngOnInit(): void {
     this._tabService.getLanistaTab().then((lanistaTab) => {
@@ -21,5 +33,35 @@ export class AppComponent {
 
   onOpenClick(): void {
     chrome.tabs.create({ url: LANISTA_BASE_URL })
+  }
+
+  onTestClick = (): void => {
+    this.expanded = !this.expanded
+    console.log('this.expandedHeight: ', this.expandedHeight)
+    let height = this.expanded ? this.expandedHeight : this.normalHeight
+    console.log('height: ', height)
+    console.log('hej frameId: ', this.iframeId)
+
+    this._scriptService.runFunction(
+      (iframeId: string, height: string) => {
+        console.log('height: ', height)
+        console.log('frameId: ', iframeId)
+        const existingElement = document.getElementById(
+          iframeId,
+        ) as HTMLIFrameElement
+        if (existingElement) {
+          console.log('existingelement: ', existingElement)
+
+          existingElement.style.height =
+            existingElement.contentWindow.document.documentElement
+              .scrollHeight + 'px'
+
+          // existingElement.style.height = '256px'
+          // existingElement.style.height = h
+          // }
+        }
+      },
+      [this.iframeId, '' + height],
+    )
   }
 }
---
+++ src/assets/scripts/inject.js
@@ -0,0 +1,37 @@
+// alert('Hello from inject.js')
+
+const iframeId = 'lanista-helper'
+const iframe = document.createElement('iframe')
+iframe.id = iframeId
+iframe.src = chrome.runtime.getURL('index.html')
+// iframe.style.backgroundColor = 'rbga(0,0,0,0.5)'
+iframe.style.position = 'fixed'
+iframe.style.zIndex = '1000'
+iframe.style.height = 'auto'
+iframe.style.minHeight = '64px'
+iframe.style.height = 'auto'
+iframe.style.width = '100%'
+iframe.style.top = '0'
+iframe.style.left = '0'
+iframe.style.border = 'none'
+// iframe.style.right = '0'
+// iframe.style.bottom = '0'
+// iframe.style.pointerEvents = 'none'
+
+// var iframe = document.createElement('p') // is a node
+// iframe.innerHTML = 'ANTON TEST'
+// document.body.appendChild(iframe)
+
+console.log('document: ', document)
+console.log('iframe: ', iframe)
+
+// document.body.appendChild(iframe)
+
+// var object = document.createElement('embed')
+// object.src = index
+
+const existingElement = document.getElementById(iframeId)
+console.log('existingElement: ', existingElement)
+if (!existingElement) {
+  document.body.appendChild(iframe)
+}
