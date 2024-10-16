898935dc314b24eae3ceb741dd97c7f429b25dac
Anton Fahlgren <antonfahlgren1994@gmail.com>
2024-10-16 16:56:05 +0200

    Cleaning up and removing unused dependencies (angular material).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
-// Angular
-// import { FlexLayoutModule } from '@angular/flex-layout'
 import { ReactiveFormsModule } from '@angular/forms'
-
-// Material
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatIconModule } from '@angular/material/icon'
-import { MatButtonModule } from '@angular/material/button'
-import { MatTooltipModule } from '@angular/material/tooltip'
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
-import { MatStepperModule } from '@angular/material/stepper'
-import { MatRadioModule } from '@angular/material/radio'
-import { MatSelectModule } from '@angular/material/select'
-import { MatSliderModule } from '@angular/material/slider'
-import { MatSnackBarModule } from '@angular/material/snack-bar'
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatListModule } from '@angular/material/list'
-import { MatDividerModule } from '@angular/material/divider'
-import { MatBottomSheetModule } from '@angular/material/bottom-sheet'
-import { MatBadgeModule } from '@angular/material/badge'
-import { MatProgressBarModule } from '@angular/material/progress-bar'
-import { MatCardModule } from '@angular/material/card'
-import { MatRippleModule } from '@angular/material/core'
-import { MatGridListModule } from '@angular/material/grid-list'
-import { MatDialogModule } from '@angular/material/dialog'
-import { MatMenuModule } from '@angular/material/menu'
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'
-import { MatButtonToggleModule } from '@angular/material/button-toggle'
-import { MatExpansionModule } from '@angular/material/expansion'
-import { MatCheckboxModule } from '@angular/material/checkbox'
 import { AppComponent } from './app.component'
 import { ChallengeComponent } from './challenge/challenge.component'
 import { PercentageLabelPipe } from './shared/pipes/percentage-label.pipe'
@@ -52,38 +20,7 @@ import { ProgressSpinnerModule } from 'primeng/progressspinner'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-
-    // Angular
     ReactiveFormsModule,
-
-    // Material
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTooltipModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatStepperModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatSliderModule,
-    MatSnackBarModule,
-    MatProgressSpinnerModule,
-    MatSidenavModule,
-    MatListModule,
-    MatDividerModule,
-    MatBottomSheetModule,
-    MatBadgeModule,
-    MatProgressBarModule,
-    MatCardModule,
-    MatRippleModule,
-    MatGridListModule,
-    MatDialogModule,
-    MatMenuModule,
-    MatSlideToggleModule,
-    MatButtonToggleModule,
-    MatExpansionModule,
-    MatCheckboxModule,
 
     // PrimeNG
     AccordionModule,
